b19bfbeec8e514312a2f1b615ac579bb9e8c7483
Taylor Schley <taylor.schley@kroger.com>
2024-06-12 14:57:54 -0500

    chore: updates

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -7,8 +7,8 @@ return {
       templates = { "core" },
     },
     keys = {
-      { "<leader>or", "<cmd>OverseerRun<cr>" },
-      { "<leader>ot", "<cmd>OverseerToggle<cr>" },
+      { "<leader>or", "<cmd>OverseerRun<cr>", { desc = "Run Overseer" } },
+      { "<leader>ot", "<cmd>OverseerToggle<cr>", { desc = "Toogle Overseer" } },
     },
   },
   {
@@ -16,6 +16,30 @@ return {
     opts = {
       defaults = {
         ["<leader>o"] = { name = "+overseer" },
+      },
+    },
+  },
+  -- Workspaces
+  -- https://github.com/natecraddock/workspaces.nvim
+  {
+    "natecraddock/workspaces.nvim",
+    opts = {},
+    config = function(opts)
+      require("workspaces").setup(opts)
+
+      LazyVim.on_load("telescope.nvim", function()
+        require("telescope").load_extension("workspaces")
+      end)
+    end,
+    keys = {
+      { "<leader>wf", "<cmd>Telescope workspaces<cr>", desc = "Find workspace" },
+    },
+  },
+  {
+    "folke/which-key.nvim",
+    opts = {
+      defaults = {
+        ["<leader>w"] = { name = "+windows/workspaces" },
       },
     },
   },
--- .config/nvim/lua/plugins/test.lua
+++ .config/nvim/lua/plugins/test.lua
@@ -6,7 +6,6 @@ return {
     dependencies = { "nvim-neotest/neotest-jest" },
     opts = {
       adapters = {
-        -- ["nx.neotest"] = {},
         ["neotest-jest"] = {
           jestCommand = "yarn test",
           jestConfigFile = function(file)
@@ -27,5 +26,31 @@ return {
         },
       },
     },
+    -- config = function(opts)
+    --   require("nx").setup_neotest(vim.tbl_extend("force", opts, {
+    --     adapters = {
+    --       ["neotest-jest"] = {
+    --         jestCommand = "yarn test",
+    --         jestConfigFile = function(file)
+    --           -- Handle non-Nx workspaces
+    --           if string.find(file, "/packages/") then
+    --             return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
+    --           end
+    --
+    --           return vim.fn.getcwd() .. "/jest.config.ts"
+    --         end,
+    --         cwd = function(file)
+    --           if string.find(file, "/packages/") then
+    --             return string.match(file, "(.-/[^/]+/)src")
+    --           end
+    --
+    --           return vim.fn.getcwd()
+    --         end,
+    --       },
+    --     },
+    --   }))
+    --
+    --   require("neotest").setup(opts)
+    -- end,
   },
 }
