56a7d5deb0addb0d1f8310a7a03cda879da41c38
Taylor Schley <taylor.schley@kroger.com>
2025-01-27 14:03:30 -0600

    feat: add window-picker

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -73,4 +73,14 @@ return {
       },
     },
   },
+  {
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    config = function()
+      require("window-picker").setup({
+        hint = "floating-big-letter",
+      })
+    end,
+  },
 }
