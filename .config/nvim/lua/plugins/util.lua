dcf249d7cfb7473d82637dd9dddb3cbf8a5b1f23
Taylor Schley <taylorschley@me.com>
2025-04-07 07:46:01 -0500

    feat: add lualine fix

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -25,6 +25,7 @@ return {
             kind = "progress",
             cond = function(message)
               local client = vim.tbl_get(message.opts, "progress", "client")
+
               return client == "vtsls"
             end,
           },
