1ae2980079ab4e047ed24a1134f48e235e27861a
Taylor Schley <taylor.schley@kroger.com>
2025-09-19 09:31:04 -0500

    feat: remove some workaround items

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -7,26 +7,4 @@ return {
     -- name = "nx",
     opts = {},
   },
-  {
-    "folke/noice.nvim",
-    opts = {
-      routes = {
-        {
-          filter = {
-            event = "lsp",
-            kind = "progress",
-            cond = function(message)
-              local client = vim.tbl_get(message.opts, "progress", "client")
-
-              return client == "vtsls"
-            end,
-          },
-          opts = {
-            -- 1 minute timeout
-            timeout = 1 * 60 * 1000,
-          },
-        },
-      },
-    },
-  },
 }
