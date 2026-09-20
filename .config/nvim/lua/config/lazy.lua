ebd6a5080450c8842a7fd4e691512c6a9ce757bc
Taylor Schley <taylorschley@me.com>
2026-09-20 10:31:52 -0500

    chore: better maangement of lazyvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup(
       { import = "lazyvim.plugins.extras.coding.yanky" },
       { import = "lazyvim.plugins.extras.editor.dial" },
       { import = "lazyvim.plugins.extras.editor.inc-rename" },
+      { import = "lazyvim.plugins.extras.editor.outline" },
       { import = "lazyvim.plugins.extras.lsp.neoconf" },
       -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
       { import = "lazyvim.plugins.extras.ui.mini-animate" },
